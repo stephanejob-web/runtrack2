11dfcbec179ea3315bc2910eda4e505c58f2b88f
stephane-job <stephane.job@laplateforme.io>
2025-09-16 16:32:29 +0200

    wip

--- jour02/job07/index.php
+++ jour02/job07/index.php
@@ -10,19 +10,34 @@
     <title>Document</title>
 </head>
 <body>
-<div>
+<div class="container">
+    <h1 class="container is-size-1 has-text-primary has-text-centered "> JEUX DE TRIANGLE</h1>
+    <input
+            class="input is-large"
+            type="number"
+            placeholder="Large input"/>
     <?php
    function triangle($hauteur) {
     echo "<pre>";
     for ($i = 1; $i <= $hauteur; $i++) {
         $espaces = str_repeat(" ", $hauteur - $i);
         $diese   = str_repeat("#", 2 * $i - 1);
-        echo $espaces . $diese . "\n";
+        echo "<h1 class='is-size-1 has-text-primary '>$espaces  $diese  </h1>";
     }
     echo "</pre>";
     }
-    triangle(3);
+    triangle(15);
     ?>
+
     </div>
 </body>
 </html>
+
+<script>
+    let num = 12
+    let change = document.querySelector("input");
+    change.addEventListener("change", function() {
+        let valeur = change.value;
+
+    })
+</script>

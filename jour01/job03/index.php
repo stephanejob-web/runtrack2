9597f908b120f22d2ad81ed964345ca8b49859cd
stephane-job <stephane.job@laplateforme.io>
2025-09-16 10:53:54 +0200

    refactoring

--- jour01/job03/index.php
+++ jour01/job03/index.php
@@ -11,29 +11,23 @@
 </head>
 <body>
 <div style="display: flex ;justify-content: center ;align-items: center ;flex-direction: column" class="container">
-<?php
-// Booléen
-$estConnecte = true;
-// Entier
-$age = 32;
-// Chaîne de caractères
-$prenom = "Laurent";
-// Nombre à virgule flottante
-$temperature = 21.5;
-// Affichage avec <br> pour aller à la ligne dans le navigateur
-echo "<h1 class='has-text-primary is-size-1'> <h1 class='has-text-primary is-size-1' > Connecté : " .($estConnecte ? "Vrai " : "Faux</h1>") . "</h1>";
-echo "<h1 class='has-text-primary is-size-1'>Âge : " . $age . "</h1>";
-echo " <h1 class='has-text-primary is-size-1'> Prénom : " . $prenom . " </h1>";
-echo " <h1 class='has-text-primary is-size-1'>Température : " . $temperature . "</h1>";
-include 'datas.php';
-?>
+    <?php
+    $estConnecte = true;
+    $age = 32;
+    $prenom = "Laurent";
+    $temperature = 21.5;
+    echo "<h1 class='has-text-primary is-size-1'> <h1 class='has-text-primary is-size-1' > Connecté : " . ($estConnecte ? "Vrai " : "Faux</h1>") . "</h1>";
+    echo "<h1 class='has-text-primary is-size-1'>Âge : " . $age . "</h1>";
+    echo " <h1 class='has-text-primary is-size-1'> Prénom : " . $prenom . " </h1>";
+    echo " <h1 class='has-text-primary is-size-1'>Température : " . $temperature . "</h1>";
+    include 'datas.php';
+    ?>
 </div>
-
 <hr>
 <div class="container">
     <table class="table">
         <caption>
-           <h1 class="has-background-success is-size-1">Les Types</h1>
+            <h1 class="has-background-success is-size-1">Les Types</h1>
         </caption>
         <thead>
         <tr>
@@ -50,6 +44,5 @@ include 'datas.php';
         </thead>
     </table>
 </div>
-
 </body>
 </html>
